a5ce10025bdd763ca7f3bb61eaaa0fdc4791820c
Vishwanathms <vishwanath.murthy@gmail.com>
2019-11-14 08:02:38 +0530

    Update Samplecode.java

--- Samplecode.java
+++ Samplecode.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]) throws InterruptedException{  
      Thread.sleep(5000);
-     System.out.println("Hello Java Update in B1 ,1 min");  
+     System.out.println("Hello Java Update in B1 ,1 min, Batch-B23");  
     }  
 }
